45065a7c9814ab87837e02a8293e977d8753a947
nikitanosov1 <nikinosov2002@mail.ru>
2023-09-06 10:45:34 +0400

    Инициализация

---
+++ src/main/java/ru/tinkoff/tictactoebottinkoffsummerpractice/TictactoeBotTinkoffSummerPracticeApplication.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.tictactoebottinkoffsummerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeBotTinkoffSummerPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactoeBotTinkoffSummerPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tinkoff/tictactoebottinkoffsummerpractice/TictactoeBotTinkoffSummerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.tictactoebottinkoffsummerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeBotTinkoffSummerPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
